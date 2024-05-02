b73098fcef4a4cb15f7805065b78c039a8a8fe2c
dauid64 <carlosdauid64br@gmail.com>
2024-05-02 18:16:01 -0300

    C03 - Tool Responses

--- src/chat.rs
+++ src/chat.rs
@@ -1,5 +1,8 @@
+use std::fmt::Display;
+
 use crate::Result;
-use async_openai::types::{ChatChoice, ChatCompletionRequestMessage, ChatCompletionRequestUserMessageArgs, CreateChatCompletionResponse};
+use async_openai::types::{ChatChoice, ChatCompletionMessageToolCall, ChatCompletionRequestAssistantMessageArgs, ChatCompletionRequestMessage, ChatCompletionRequestToolMessage, ChatCompletionRequestToolMessageArgs, ChatCompletionRequestUserMessageArgs, ChatCompletionTool, ChatCompletionToolArgs, CreateChatCompletionResponse, FunctionObject};
+use serde_json::Value;
 
 pub fn user_msg(content: impl Into<String>) -> Result<ChatCompletionRequestMessage> {
     let msg = ChatCompletionRequestUserMessageArgs::default()
@@ -7,6 +10,33 @@ pub fn user_msg(content: impl Into<String>) -> Result<ChatCompletionRequestMessa
         .build()?;
 
     Ok(msg.into())
+}
+
+pub fn tool_response_msg(tool_call_id: String, content: impl Display) -> Result<ChatCompletionRequestMessage> {
+    let msg = ChatCompletionRequestToolMessageArgs::default()
+        .tool_call_id(tool_call_id)
+        .content(content.to_string())
+        .build()?;
+    Ok(msg.into())
+}
+
+pub fn tool_calls_msg(tool_calls: Vec<ChatCompletionMessageToolCall>) -> Result<ChatCompletionRequestMessage> {
+    let msg = ChatCompletionRequestAssistantMessageArgs::default()
+        .tool_calls(tool_calls)
+        .build()?;
+
+    Ok(msg.into())
+}
+
+pub fn tool_fn(name: impl Into<String>, description: impl Into<String>, parameters: Value) -> Result<ChatCompletionTool> {
+    let tool = ChatCompletionToolArgs::default()
+        .function(FunctionObject {
+            name: name.into(),
+            description: Some(description.into()),
+            parameters: Some(parameters),
+        })
+        .build()?;
+    Ok(tool)
 }
 
 pub fn first_choice(chat_response: CreateChatCompletionResponse) -> Result<ChatChoice> {
